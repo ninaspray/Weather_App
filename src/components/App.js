c17c54afb28423921aa1d7791688674f82f545ba
ninaspray <ninaspray@gmail.com>
2021-04-11 16:31:22 +0100

    Data Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import '..//styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>Weather App </h1>
     </div>
   );
 }
