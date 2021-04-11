ec982e8549012c47542370d01640f8bbea02a742
ninaspray <ninaspray@gmail.com>
2021-04-11 17:30:07 +0100

    Added Testing folder and files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import LocationDetails from './LocationDetails';
+import PropTypes from 'prop-types';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <h1>Weather App </h1>
+      <LocationDetails city={props.location.city} country={props.location.country}/>
     </div>
   );
 }
 
+
+
+
 export default App;
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Weather App/i);
+
+test('renders h1 element', () => {
+    render(<App />);
+  const linkElement = screen.getByText('Weather App');
   expect(linkElement).toBeInTheDocument();
 });
