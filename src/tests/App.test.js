66852e9c7e5a829dfaac6332654a429549104e7b
ninaspray <ninaspray@gmail.com>
2021-04-18 13:25:00 +0100

    Fixed tests for summaries by refactorign the App.test file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,11 +4,14 @@ import App from "../components/App";
 describe("App", () => {
   it("renders city and country", () => {
     const validProps = {
+        location: {
       city: "MockCity",
       country: "MockCountry",
+    },
+    forecasts: [],
     };
 
-    render(<App location={validProps} />);
+    render(<App forecasts={validProps.forecasts} location={validProps.location} />);
     const copy = screen.getByText(/MockCity, MockCountry/i);
     expect(copy).toBeInTheDocument();
   });
