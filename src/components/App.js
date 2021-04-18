b3c9a857df52237adc8419e1d24613aba655733c
ninaspray <ninaspray@gmail.com>
2021-04-18 19:17:55 +0100

    Added State

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LocationDetails from './LocationDetails';
 import PropTypes from 'prop-types';
 import ForecastSummaries from './ForecastSummaries';
@@ -9,6 +9,9 @@ import '../styles/App.css';
 
 
 const App = ({ forecasts, location }) => {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(forecast => forecast.date === selectedDate);
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
@@ -25,7 +28,7 @@ App.propTypes = {
     PropTypes.shape({
       date: PropTypes.number,
       description: PropTypes.string,
-      icon: PropTypes.number,
+      icon: PropTypes.string,
       tempreture: PropTypes.shape({
         max: PropTypes.number,
         min: PropTypes.number,
