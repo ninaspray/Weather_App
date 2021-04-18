3d6a4a15dc83e714f6df8cd9b23e2c8f5ffb8597
ninaspray <ninaspray@gmail.com>
2021-04-18 12:33:44 +0100

    Deconstructured props as params

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import LocationDetails from './LocationDetails';
 import PropTypes from 'prop-types';
+import ForecastSummaries from './LocationDetails';
 
-const App = (props) => {
-  const { location } = props;
+const App = ({ forecasts, location }) => {
   return (
     <div className="App">
       <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 };
@@ -14,6 +15,17 @@ const App = (props) => {
 export default App;
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.number,
+      tempreture: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
